1976437ec4065840e2d54ad170b9a7afe6ade41a
Thijs De Paepe <Thijs.dp@gmail.com>
2022-03-09 21:56:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ if [ ! -z "${SYSTEM_DISK}" ]; then
   mount ${SYSTEM_DISK}${ROOT_PART} ${CHROOT_PATH}
   mkdir ${CHROOT_PATH}/boot
   mount ${SYSTEM_DISK}${EFI_PART} ${CHROOT_PATH}/boot
-  genfstab -U ${CHROOT_PATH} >> ${CHROOT_PATH}/etc/fstab
+  genfstab -U ${CHROOT_PATH} > ${CHROOT_PATH}/etc/fstab
 else
   BUILD_IMG=${SYSTEM_NAME}-build.img
 
